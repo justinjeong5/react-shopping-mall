5fb3280f495c6b4dfd4e8a4bee9f1f133ec503e3
justinjeong <justin.jeong5@gmail.com>
2020-12-15 14:22:08 +0900

    feat(server): Product model & api "/api/product/uploadProduct"

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { User } = require('../models/User')
 const { Product } = require('../models/Product')
 const multer = require('multer');
 
@@ -29,9 +30,31 @@ var upload = multer({ storage: storage }).single('file')
 
 router.post('/uploadImage', auth, (req, res) => {
   upload(req, res, error => {
-    if (error) return res.status(400).json({ code: 'MulterError', message: '이미지를 업로드하는 과정에서 문제가 발생했습니다.' })
+    if (error) {
+      console.error(error)
+      return res.status(400).json({ code: 'MulterError', message: '이미지를 업로드하는 과정에서 문제가 발생했습니다.' })
+    }
     res.status(200).json({ success: true, image: { image: res.req.file.path, fileName: res.req.file.filename } });
   })
+})
+
+router.post('/uploadProduct', auth, (req, res) => {
+  User.findOne({ "_id": req.body.writer },
+    (error, user) => {
+      if (error) {
+        console.error(error)
+        return res.status(401).json({ code: 'DatabaseError', message: '사용자를 데이터베이스에서 찾을 수 없습니다.' })
+      }
+      User.getIndex
+      const product = new Product(req.body)
+      product.save((error, product) => {
+        if (error) {
+          console.error(error)
+          return res.status(400).json({ code: 'DatabaseError', message: '상품을 등록하는 과정에서 문제가 발생했습니다.' })
+        }
+        res.status(200).json({ success: true, product });
+      })
+    })
 })
 
 module.exports = router
